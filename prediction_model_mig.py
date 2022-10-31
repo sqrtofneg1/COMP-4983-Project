d8e55f9f2c379a067ff2172f4f71ec97a82969e1
Miguel Marasigan <miguelmarasigan55@gmail.com>
2022-10-31 14:08:18 -0700

    Setup for submission and implemented general idea, requires bug fixes to run

--- prediction_model_mig.py
+++ prediction_model_mig.py
@@ -1,11 +1,33 @@
 import numpy as np
 import pandas as pd
-from create_submission_csv import create_submission as submit
+from create_submission_csv import create_submission
 from data_preprocessing import load
 from sklearn.linear_model import LinearRegression, Ridge
 
-# Todolater: Polyvals? Run different degrees most likely. Also try Ridge regressions later.
 
-# Submission 1
+#do later: find the most important features and implememnt polyval onto them, arbitrary degree?
+#solve for best degree if have time
+class TestModel:
 
-# Submission 2
+    def __init__(self, degree):
+        self.degree = degree
+
+        claim_amount_data = pd.read_csv("datasets/trainingset_claim_amounts_only.csv")
+        claim_amount_features = claim_amount_data.drop("ClaimAmount", axis=1, inplace=False)
+        claim_amount_labels = claim_amount_data.loc[:, "ClaimAmount"]
+
+        self.reg = np.polyfit(x=claim_amount_features, y=claim_amount_labels, deg=self.degree)
+
+        self.predict(pd.read_csv("datasets/trainingset_claim_amounts_only.csv").drop("ClaimAmount", axis=1, inplace=False))
+        self.predict(pd.read_csv("datasets/trainingset.csv").drop("ClaimAmount", axis=1, inplace=False))
+
+    def predict(self, features):
+        prediction = self.reg.polyval(features)
+        return prediction
+
+
+deg5 = TestModel(5)
+deg25 = TestModel(25)
+
+create_submission(deg5, 1, 5, True)
+create_submission(deg25, 1, 6, True)
