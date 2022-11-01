c916933c9fdaabbb6dfba149edec51c902635f5b
Vincent-On <77428769+Vincent-On@users.noreply.github.com>
2022-11-01 09:06:58 -0700

    changed codes to import

--- prediction_model_vincent.py
+++ prediction_model_vincent.py
@@ -4,6 +4,7 @@ from create_submission_csv import create_submission
 from data_preprocessing import load
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
+import stat_functions as stats
 
 boolean_answers = pd.read_csv("datasets/trainingset_boolean_claim_amount.csv").loc[:, "ClaimAmount"]
 num_expected_claim_amounts = 3335
@@ -48,7 +49,7 @@ class TestModelLinearRegression:
                 predictions_claim_or_not[i] = 0
             else:
                 predictions_claim_or_not[i] = 1
-        check_claim_or_not(predictions_claim_or_not)  # only fires on trainingset
+        stats.check_claim_or_not(predictions_claim_or_not)  # only fires on trainingset
 
         predictions_claim_amount_raw = self.claim_model.predict(features)
         predictions_claim_amount = [0] * len(features)
@@ -57,47 +58,9 @@ class TestModelLinearRegression:
                 predictions_claim_amount[i] = 0
             else:
                 predictions_claim_amount[i] = predictions_claim_amount_raw[i]
-        check_claim_amount_mae(predictions_claim_amount)  # only fires on trainingset_claim_amounts_only
-        check_overall_mae(predictions_claim_amount)
+        stats.check_claim_amount_mae(predictions_claim_amount)  # only fires on trainingset_claim_amounts_only
+        stats.check_overall_mae(predictions_claim_amount)
         return predictions_claim_amount
-
-
-def check_claim_or_not(preds):
-    if len(preds) != len(boolean_answers):
-        return
-    false_positives = 0
-    missed_positives = 0
-    for i in range(len(preds)):
-        if preds[i] == 0 and boolean_answers[i] == 1:
-            missed_positives += 1
-        if preds[i] == 1 and boolean_answers[i] == 0:
-            false_positives += 1
-    print(" *** ClaimAmount Identification Stats ***")
-    print(f"  False positives: {false_positives}")
-    print(f"  Percentage false positive: {false_positives / (len(preds) - num_expected_claim_amounts) * 100:.3f}%")
-    print(f"  Missed positives: {missed_positives} out of {num_expected_claim_amounts}")
-    print(
-        f"  Percentage correctly identified: {(num_expected_claim_amounts - missed_positives) / num_expected_claim_amounts * 100:.3f}%")
-    print(f"  Overall correct percentage: {(len(preds) - false_positives - missed_positives) / len(preds) * 100:.3f}%")
-    print()
-
-
-def check_claim_amount_mae(preds):
-    if len(preds) != len(claim_amounts_answers):
-        return
-    print(" *** ClaimAmount Value Prediction Stats ***")
-    mae = np.mean(abs(preds - claim_amounts_answers))
-    print(f"  MAE: {mae}")
-    print()
-
-
-def check_overall_mae(preds):
-    if len(preds) != len(all_data_answers):
-        return
-    print(" *** Overall Prediction Stats ***")
-    mae = np.mean(abs(preds - all_data_answers))
-    print(f"  Overall MAE: {mae}")
-    print()
 
 
 def run():
