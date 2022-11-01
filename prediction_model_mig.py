e34bd881dcab08b5b7c0c884684c41309d61ede1
migu55 <miguelmarasigan55@gmail.com>
2022-10-31 19:52:56 -0700

    Polyvals based on most important feature - degrees 5 and 25 used

--- prediction_model_mig.py
+++ prediction_model_mig.py
@@ -16,14 +16,31 @@ class TestModel:
         claim_amount_features = claim_amount_data.drop("ClaimAmount", axis=1, inplace=False)
         claim_amount_labels = claim_amount_data.loc[:, "ClaimAmount"]
 
-        self.reg = np.polyfit(x=claim_amount_features, y=claim_amount_labels, deg=self.degree)
+        best_feature = self.find_best_feature(claim_amount_features, claim_amount_labels)
 
-        self.predict(pd.read_csv("datasets/trainingset_claim_amounts_only.csv").drop("ClaimAmount", axis=1, inplace=False))
-        self.predict(pd.read_csv("datasets/trainingset.csv").drop("ClaimAmount", axis=1, inplace=False))
+        best_feature_col = claim_amount_data.loc[:, f"feature{best_feature}"]
 
-    def predict(self, features):
-        prediction = self.reg.polyval(features)
-        return prediction
+        self.reg = np.polyfit(x=best_feature_col, y=claim_amount_labels, deg=self.degree)
+
+        self.predict(pd.read_csv("datasets/trainingset_claim_amounts_only.csv"))
+
+    def predict(self, dataset):
+        best_feature_col = dataset.loc[:, f"feature1"]
+        prediction = np.polyval(self.reg, best_feature_col)
+        df = pd.DataFrame()
+        df['ClaimAmount']=pd.Series(prediction)
+        return df
+
+    def find_best_feature(self, feats, labes):
+        lg = LinearRegression()
+        lg.fit(feats, labes)
+        importance = lg.coef_
+        scores = []
+        for i, v in enumerate(importance):
+            # print('Feature: %0d, Score: %5f' % (i, np.abs(v)))
+            scores.append(np.abs(v))
+        return scores.index(max(scores))
+
 
 
 deg5 = TestModel(5)
