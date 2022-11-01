3c384a33d87535dc7b60ff3e6390ad0c3665a183
Sunmin Choi <sunminchoi1214@gmail.com>
2022-10-31 23:18:36 -0700

    Add tensorflow for boolean part prediction along with new model. Calculate tolerance if value not provided.

--- prediction_model_sunmin.py
+++ prediction_model_sunmin.py
@@ -4,6 +4,8 @@ from create_submission_csv import create_submission
 from data_preprocessing import load
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
+import prediction_model_sunmin_tensorflow as tensorflow_model
+
 
 boolean_answers = pd.read_csv("datasets/trainingset_boolean_claim_amount.csv").loc[:, "ClaimAmount"]
 num_expected_claim_amounts = 3335
@@ -22,9 +24,9 @@ class TestModelZeroes:
         return [0] * len(testdata)
 
 
-class TestModelLinearRegression:
+class TestModelLinearRegression:  # data normalization only on continuous data
 
-    def __init__(self, tolerance):
+    def __init__(self, tolerance=None):
         self.tolerance = tolerance
         lin_reg = LinearRegression()
         boolean_data = pd.read_csv("datasets/trainingset_boolean_claim_amount.csv")
@@ -41,15 +43,80 @@ class TestModelLinearRegression:
         claim_amount_labels = claim_amount_data.loc[:, "ClaimAmount"]
         self.claim_amount_model = lin_reg_2.fit(claim_amount_features, claim_amount_labels)
 
+        self.calculate_tolerance()
+
         # For getting the stats for check_claim_amount_mae
         self.predict(
             pd.read_csv("datasets/trainingset_claim_amounts_only.csv").drop("ClaimAmount", axis=1, inplace=False))
         # For getting the stats for check_claim_or_not
         self.predict(pd.read_csv("datasets/trainingset.csv").drop("ClaimAmount", axis=1, inplace=False))
 
+    def calculate_tolerance(self):
+        features = pd.read_csv("datasets/trainingset.csv").drop("ClaimAmount", axis=1, inplace=False)
+        features.loc[:, continuous] = StandardScaler().fit_transform(features.loc[:, continuous])
+        predictions_for_tolerance = self.claim_or_not_model.predict(features)
+        if self.tolerance is None:
+            sorted_arr = sorted(predictions_for_tolerance)
+            print(np.percentile(sorted_arr, 95))
+            self.tolerance = sorted_arr[-num_expected_claim_amounts]
+        print(f"TOLERANCE: {self.tolerance}")
+
     def predict(self, features):
         features.loc[:, continuous] = StandardScaler().fit_transform(features.loc[:, continuous])
         predictions_claim_or_not_raw = self.claim_or_not_model.predict(features)
+        predictions_claim_or_not = [0] * len(features)
+        for i in range(len(features)):
+            if predictions_claim_or_not_raw[i] < self.tolerance:  # tune tolerance value to be closer to actual ratio
+                predictions_claim_or_not[i] = 0
+            else:
+                predictions_claim_or_not[i] = 1
+        check_claim_or_not(predictions_claim_or_not)  # only fires on trainingset
+
+        predictions_claim_amount_raw = self.claim_amount_model.predict(features)
+        predictions_claim_amount = [0] * len(features)
+        for i in range(len(features)):
+            if predictions_claim_or_not[i] == 0:  # tune tolerance value to be closer to actual ratio
+                predictions_claim_amount[i] = 0
+            else:
+                predictions_claim_amount[i] = predictions_claim_amount_raw[i]
+        check_claim_amount_mae(predictions_claim_amount)  # only fires on trainingset_claim_amounts_only
+        check_overall_mae(predictions_claim_amount)
+        return predictions_claim_amount
+
+
+class TestModelTFLinearRegression:  # data normalization on all data
+
+    def __init__(self, tolerance=None):
+        self.tolerance = tolerance
+        self.claim_or_not_model = tensorflow_model.get_tensorflow_model_boolean()
+
+        lin_reg_2 = LinearRegression()
+        claim_amount_data = pd.read_csv("datasets/trainingset_claim_amounts_only.csv")
+        claim_amount_features = claim_amount_data.drop("ClaimAmount", axis=1, inplace=False)
+        claim_amount_features = StandardScaler().fit_transform(claim_amount_features)
+        claim_amount_labels = claim_amount_data.loc[:, "ClaimAmount"]
+        self.claim_amount_model = lin_reg_2.fit(claim_amount_features, claim_amount_labels)
+
+        self.calculate_tolerance()
+
+        # For getting the stats for check_claim_amount_mae
+        self.predict(
+            pd.read_csv("datasets/trainingset_claim_amounts_only.csv").drop("ClaimAmount", axis=1, inplace=False))
+        # For getting the stats for check_claim_or_not
+        self.predict(pd.read_csv("datasets/trainingset.csv").drop("ClaimAmount", axis=1, inplace=False))
+
+    def calculate_tolerance(self):
+        features = pd.read_csv("datasets/trainingset.csv").drop("ClaimAmount", axis=1, inplace=False)
+        features = StandardScaler().fit_transform(features)
+        predictions_for_tolerance = self.claim_or_not_model.predict(features)[:, 1]
+        if self.tolerance is None:
+            sorted_arr = sorted(predictions_for_tolerance)
+            self.tolerance = np.percentile(sorted_arr, 93)
+        print(f"TOLERANCE: {self.tolerance}")
+
+    def predict(self, features):
+        features = StandardScaler().fit_transform(features)
+        predictions_claim_or_not_raw = self.claim_or_not_model.predict(features)[:, 1]
         predictions_claim_or_not = [0] * len(features)
         for i in range(len(features)):
             if predictions_claim_or_not_raw[i] < self.tolerance:  # tune tolerance value to be closer to actual ratio
@@ -84,8 +151,8 @@ def check_claim_or_not(preds):
     print(f"  False positives: {false_positives}")
     print(f"  Percentage false positive: {false_positives / (len(preds) - num_expected_claim_amounts) * 100:.3f}%")
     print(f"  Missed positives: {missed_positives} out of {num_expected_claim_amounts}")
-    print(
-        f"  Percentage correctly identified: {(num_expected_claim_amounts - missed_positives) / num_expected_claim_amounts * 100:.3f}%")
+    print(f"  Percentage correctly identified: "
+          f"{(num_expected_claim_amounts - missed_positives) / num_expected_claim_amounts * 100:.3f}%")
     print(f"  Overall correct percentage: {(len(preds) - false_positives - missed_positives) / len(preds) * 100:.3f}%")
     print()
 
@@ -116,7 +183,15 @@ def run():
     create_submission(TestModelZeroes, 1, 1, False)
 
     print("***** LINEAR REGRESSION *****")
-    lin_reg_model = TestModelLinearRegression(0.1)
+    lin_reg_model = TestModelLinearRegression()
     create_submission(lin_reg_model, 1, 2, True)
 
+    print("***** TF THEN LINEAR REGRESSION *****")
+    tf_lin_reg_model = TestModelTFLinearRegression()
+    create_submission(tf_lin_reg_model, 1, 9, True)
+
 # Decision tree for the boolean part?
+
+
+if __name__ == "__main__":
+    run()
---
+++ prediction_model_sunmin_tensorflow.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+from tensorflow.keras import Input
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD
+
+
+def get_tensorflow_model_boolean():
+    boolean_data = pd.read_csv("datasets/trainingset_boolean_claim_amount.csv")
+    boolean_features = boolean_data.drop("ClaimAmount", axis=1, inplace=False)
+    boolean_features = StandardScaler().fit_transform(boolean_features)
+    boolean_labels = boolean_data.loc[:, "ClaimAmount"]
+
+    num_features = boolean_features.shape[1]
+
+    model = Sequential()
+    model.add(Input(shape=(num_features,)))
+    model.add(Dense(800, activation="tanh"))
+    model.add(Dense(100, activation="tanh"))
+    model.add(Dense(2, activation="softmax"))
+
+    model.summary()
+
+    optimizer = SGD(learning_rate=0.1)
+    model.compile(optimizer=optimizer, loss="sparse_categorical_crossentropy", metrics="accuracy")
+
+    model.fit(boolean_features, boolean_labels, epochs=10, verbose=1)
+
+    return model
