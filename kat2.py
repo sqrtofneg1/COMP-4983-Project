bd6097b06c7ab250dc41440e8941916b9b7bd710
Kat Sukkau <Katsukkau@gmail.com>
2022-11-06 09:06:31 -0800

    added 2 further models on kat2.py

--- kat2.py
+++ kat2.py
@@ -15,25 +15,16 @@ claim_amounts_answers = pd.read_csv("datasets/trainingset_claim_amounts_only.csv
 all_data = pd.read_csv("datasets/trainingset.csv")
 all_data_answers = all_data.loc[:, "ClaimAmount"]
 boolean_data = pd.read_csv('datasets/trainingset_boolean_claim_amount.csv')
-X = boolean_data.drop("ClaimAmount", axis=1, inplace=False)
-y = boolean_data.loc[:, "ClaimAmount"]
 continuous_data = pd.read_csv("datasets/trainingset_claim_amounts_only.csv")
 continuous_features = continuous_data.drop("ClaimAmount", axis=1, inplace=False)
 continuous_labels = continuous_data.loc[:, "ClaimAmount"]
 
 
-undersample = RandomUnderSampler(sampling_strategy=0.5)
-oversample = RandomOverSampler(sampling_strategy=0.5)
-
-X_over, y_over = oversample.fit_resample(X, y)
-X_under, y_under = undersample.fit_resample(X, y)
-
-class UnderSamplerDecisionTree:
-
-    def __init__(self, tolerance):
+class DecisionTree:
+    def __init__(self, tolerance, X, y):
         self.tolerance = tolerance
         treeclass = DecisionTreeClassifier()
-        self.boolean_claim_model = treeclass.fit(X_under, y_under)
+        self.boolean_claim_model = treeclass.fit(X, y)
         treereg = DecisionTreeRegressor()
         self.claim_amount_model = treereg.fit(continuous_features, continuous_labels)
 
@@ -64,8 +55,58 @@ class UnderSamplerDecisionTree:
         stats.check_overall_mae(predictions_claim_amount)
         return predictions_claim_amount
 
-# def run():
-print("***** tree *****")
-model = UnderSamplerDecisionTree(0.1)
-create_submission(model, 2, 3, True)
 
+class OverSamplerData:
+
+    def __init__(self):
+        X = boolean_data.drop("ClaimAmount", axis=1, inplace=False)
+        y = boolean_data.loc[:, "ClaimAmount"]
+        oversample = RandomOverSampler(sampling_strategy=0.5)
+        self.X, self.y = oversample.fit_resample(X, y)
+
+    def values(self):
+        return self.X, self.y
+
+
+class UnderSamplerData:
+
+    def __init__(self):
+        X = boolean_data.drop("ClaimAmount", axis=1, inplace=False)
+        y = boolean_data.loc[:, "ClaimAmount"]
+        undersample = RandomUnderSampler(sampling_strategy=0.5)
+        self.X, self.y = undersample.fit_resample(X, y)
+
+    def values(self):
+        return self.X, self.y
+
+
+class UnderOverSamplerData:
+
+    def __init__(self):
+        X = boolean_data.drop("ClaimAmount", axis=1, inplace=False)
+        y = boolean_data.loc[:, "ClaimAmount"]
+        over = RandomOverSampler(sampling_strategy=0.1)
+        under = RandomUnderSampler(sampling_strategy=0.5)
+        X, y = over.fit_resample(X, y)
+        self.X, self.y = under.fit_resample(X, y)
+
+    def values(self):
+        return self.X, self.y
+
+
+# def run():
+print("***** UNDERSAMPLE WITH DECISON TREE *****")
+under = UnderSamplerData()
+X_under, y_under = under.values()
+model = DecisionTree(0.1, X_under, y_under)
+create_submission(model, 2, 3, True)
+print("***** OVERSAMPLE WITH DECISION TREE *****")
+over = OverSamplerData()
+X_over, y_over = over.values()
+model = DecisionTree(0.1, X_over, y_over)
+create_submission(model, 2, 4, True)
+print("***** UNDER-OVERSAMPLE WITH DECISION TREE *****")
+under_over = UnderOverSamplerData()
+X_under_over, y_under_over = under_over.values()
+model = DecisionTree(0.1, X_under_over, y_under_over)
+create_submission(model, 2, 5, True)
