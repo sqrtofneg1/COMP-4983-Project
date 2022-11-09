22e765f4b7f793db1aa7d6e2b3758409f5a43f23
Kat Sukkau <Katsukkau@gmail.com>
2022-11-09 13:20:35 -0800

    added tuning options for testing in examples of over under sampling

--- examples_of_overunder_sampling.py
+++ examples_of_overunder_sampling.py
@@ -1,4 +1,5 @@
 from numpy import mean
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.tree import DecisionTreeClassifier
@@ -12,23 +13,25 @@ y = data.loc[:, "ClaimAmount"]
 
 # Random oversampling of boolean dataset
 # define pipeline
-steps = [('over', RandomOverSampler()), ('model', DecisionTreeClassifier())]
+over = RandomOverSampler(sampling_strategy=0.1)
+steps = [('over', over), ('model', DecisionTreeClassifier())]
 pipeline = Pipeline(steps=steps)
 # evaluate pipeline
 cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 scores = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
 score = mean(scores)
-print('F1 Score: %.3f' % score)
+print('Over with decision tree F1 Score: %.3f' % score)
 
 # Random undersampling of boolean dataset
 # define pipeline
-steps = [('under', RandomUnderSampler()), ('model', DecisionTreeClassifier())]
+under = RandomUnderSampler(sampling_strategy=0.5)
+steps = [('under', under), ('model', DecisionTreeClassifier())]
 pipeline = Pipeline(steps=steps)
 # evaluate pipeline
 cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 scores = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
 score = mean(scores)
-print('F1 Score: %.3f' % score)
+print('Under with decision tree F1 Score: %.3f' % score)
 
 # Random oversampling and undersampling combined
 # define pipeline
@@ -40,5 +43,38 @@ pipeline = Pipeline(steps=steps)
 cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 scores = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
 score = mean(scores)
-print('F1 Score: %.3f' % score)
+print('Combined with decision tree F1 Score: %.3f' % score)
 
+# Random oversampling of boolean dataset
+# define pipeline
+over = RandomOverSampler(sampling_strategy=0.1)
+steps = [('over', over), ('model', RandomForestClassifier(n_estimators=100, random_state=100))]
+pipeline = Pipeline(steps=steps)
+# evaluate pipeline
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+scores = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
+score = mean(scores)
+print('Over with random forest F1 Score: %.3f' % score)
+
+# Random undersampling of boolean dataset
+# define pipeline
+under = RandomUnderSampler(sampling_strategy=0.5)
+steps = [('under', under), ('model', RandomForestClassifier(n_estimators=100, random_state=100))]
+pipeline = Pipeline(steps=steps)
+# evaluate pipeline
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+scores = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
+score = mean(scores)
+print('Under with random forest F1 Score: %.3f' % score)
+
+# Random oversampling and undersampling combined
+# define pipeline
+over = RandomOverSampler(sampling_strategy=0.1)
+under = RandomUnderSampler(sampling_strategy=0.5)
+steps = [('o', over), ('u', under), ('m', RandomForestClassifier(n_estimators=100, random_state=100))]
+pipeline = Pipeline(steps=steps)
+# evaluate pipeline
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+scores = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
+score = mean(scores)
+print('Combined with random forest F1 Score: %.3f' % score)
